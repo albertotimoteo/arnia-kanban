d700eaf303fbbb9dbc59a86026a4577a978dfd49
albertotimoteobp <albertotimoteo@bemprotegetech.com.br>
2024-07-29 15:11:40 -0300

    feat: added access to all origins

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const checkKey = require("./middlewares/checkKey")
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(cors())
+app.use(cors({ origin: "*" }))
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
